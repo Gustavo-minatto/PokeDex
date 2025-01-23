8a1e97a351c9e6c4b41e78a482b1b07be6564c62
Gustavo Minatto <gustavominatto23@gmail.com>
2025-01-23 13:11:49 -0300

    Svg

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,10 @@
 import { Container } from './styles';
+import pokemonLogo from '../../../public/pokemon.svg';
 
 export function Header() {
   return (
     <Container>
-      <img src="/public/pokemon.svg" alt="Pokémon" />
+      <img src={pokemonLogo} alt="Pokémon" />
       <p>Documentação</p>
     </Container>
   )
